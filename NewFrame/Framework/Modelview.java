b12346b50016988e38797c60e886384b5da2f06c
LeslieRakotondralala <rakotondralalaleslie@gmail.com>
2023-06-06 13:48:01 +0300

    Commit sprint5

---
+++ NewFrame/Framework/Modelview.java
@@ -0,0 +1,12 @@
+package etu1920.framework;
+
+public class Modelview {
+    String view;
+
+    public String getView() {
+        return view;
+    }
+    public void setView(String view) {
+        this.view = view;
+    }
+}
