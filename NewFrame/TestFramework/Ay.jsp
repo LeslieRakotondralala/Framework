a95b40b3126293b4755fcca01275c13fcb876199
LeslieRakotondralala <rakotondralalaleslie@gmail.com>
2023-06-07 11:07:15 +0300

    Commit sprint7

---
+++ NewFrame/TestFramework/Ay.jsp
@@ -0,0 +1,13 @@
+<html>
+
+<body>
+
+
+    <form action="get-form" method="post">
+        <input type="text" name="nom" id="">
+        <input type="number" name="t" id="">
+        <input type="submit" value="Ok">
+    </form>
+</body>
+
+</html>
--- NewFrame/TestFramework/Test/Emp.java
+++ NewFrame/TestFramework/Test/Emp.java
@@ -1,10 +1,41 @@
 package Test;
-import etu1920.framework.Url;
+
+import etu1987.framework.Url;
+import etu1987.framework.Modelview;
 
 public class Emp {
-    
-    @Url(url="find-All")
-    public void FindAll() {
-        System.out.println(" find_all ");
+    String nom;
+    Integer t;
+
+    public void setT(Integer t) {
+        this.t = t;
     }
+    public Integer getT() {
+        return t;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getNom() {
+        return nom;
+    }
+    @Url(url="find-All")
+    public Modelview FindAll() {
+        Modelview m = new Modelview();
+        m.setView("Ay.jsp");
+        int u = 23;
+        m.addItem("data", u);
+        System.out.println(" find_all ");
+        return m;
+    }
+    
+    @Url(url="get-form")
+    public Modelview getForm() {
+        Modelview m = new Modelview();
+        m.setView("Ay.jsp");
+        System.out.println(this.getNom());
+        return m;
+    }
+
 }
