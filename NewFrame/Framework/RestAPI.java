8512b9a8371d087c2e59e098acf20801056fc32d
LeslieRakotondralala <rakotondralalaleslie@gmail.com>
2023-06-18 10:00:55 +0300

    Commit Sprint 14

---
+++ NewFrame/Framework/RestAPI.java
@@ -0,0 +1,10 @@
+
+package etu1920.framework;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface RestAPI {
+}
--- NewFrame/build.bat
+++ NewFrame/build.bat
@@ -1,9 +1,9 @@
 @REM For the framework compilation
 
-set "FRAMEWORK_FOLDER=C:\SPRINT\Framew-Sprint4\NewFrame\"
-set FRAMEWORK="C:\SPRINT\Framew-Sprint4\NewFrame\Framework"
+set "FRAMEWORK_FOLDER=C:\SPRINT\Servlet_generalization-sprint14\Servlet_generalization-sprint14\NewFrame\NewFrame\"
+set FRAMEWORK="C:\SPRINT\Servlet_generalization-sprint14\Servlet_generalization-sprint14\NewFrame\NewFrame\Framework"
 set TOMCAT="C:\Program Files\Apache Software Foundation\Tomcat 10.0\webapps"
-set TESTFRAMEWORK="C:\SPRINT\Framew-Sprint4\NewFrame\TestFramework"
+set TESTFRAMEWORK="C:\SPRINT\Servlet_generalization-sprint14\Servlet_generalization-sprint14\NewFrame\NewFrame\TestFramework"
 
 javac -d %FRAMEWORK%\bin %FRAMEWORK%\\*.java
 cd %FRAMEWORK%\\bin\\
@@ -12,7 +12,7 @@ cd %FRAMEWORK_FOLDER%
 
 copy framework.jar %TESTFRAMEWORK%\lib
 
-@REM For the test compilation
+@REM For the test compilation 
 
 mkdir temp
 mkdir temp\\WEB-INF
@@ -23,6 +23,7 @@ xcopy %TESTFRAMEWORK%\\*.jsp temp
 xcopy %TESTFRAMEWORK%\\lib temp\\WEB-INF\\lib
 javac -cp temp\\WEB-INF\\lib\\framework.jar -d temp\\WEB-INF\\classes %TESTFRAMEWORK%\\Test\\*.java
 xcopy %TESTFRAMEWORK%\\*.xml temp\\WEB-INF
+
 @REM rmdir -r temp
 
 cd temp
